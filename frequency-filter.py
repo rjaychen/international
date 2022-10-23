d3d3d60c47d979fe46ceba75233045cc63b8facd
David L. Yao <david.yao313@duke.edu>
2022-10-23 02:42:43 -0400

    gets rid of exterior text

--- frequency-filter.py
+++ frequency-filter.py
@@ -5,38 +5,25 @@ import keybert
 from keybert import KeyBERT
 kw_model = KeyBERT()
 
-def filter_stopwords(text: str) -> str:
-    text = text.lower()
-    word_tokens = word_tokenize(example_sent)
+
+
+def filter_stopwords(text_pathname: str) -> str:
+    with open(text_pathname) as f:
+        transcript = f.read()
+    stop_words = set(stopwords.words('english'))
+    stop_words.add(",")
+    stop_words.add(".")
+    stop_words.add("ok")
+    stop_words.add(".")
+    transcript = transcript.lower()
+    word_tokens = word_tokenize(transcript)
     filtered_sentence = [w for w in word_tokens if not w.lower() in stop_words]
-
     filtered_sentence = []
-
     for w in word_tokens:
         if w not in stop_words:
             filtered_sentence.append(w)
-    return filtered_sentence
-
-def find_frequency(input: list):
-    dictionary = {}
-    for word in input:
-        if word in dictionary:
-            dictionary[word] += 1
-        else:
-            dictionary[word] = 1
-    return dictionary
+    return kw_model.extract_keywords(transcript, keyphrase_ngram_range=(3, 3), stop_words=stop_words,
+                              use_maxsum=True, nr_candidates=20, top_n=5)
 
 
-stop_words = set(stopwords.words('english'))
-stop_words.add(",")
-stop_words.add(".")
-stop_words.add("ok")
-stop_words.add(".")
 
-
-with open("transcript.txt") as f:
-    transcript = f.read()
-example_sent = transcript.lower()
-to_sort = find_frequency(filter_stopwords(example_sent))
-keywords = kw_model.extract_keywords(example_sent)
-print(kw_model.extract_keywords(example_sent, keyphrase_ngram_range=(2, 2), stop_words=None))
--- keyword_extract.py
+++
@@ -1,7 +0,0 @@
-import keybert
-from keybert import KeyBERT
-
-
-kw_model = KeyBERT()
-keywords = kw_model.extract_keywords(doc)
-print(kw_model.extract_keywords(doc, keyphrase_ngram_range=(1, 1), stop_words=None))
