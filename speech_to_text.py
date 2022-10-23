f1500951d36db40cf038a4e0b41307d437115e33
Yi-ChyunWong <61467580+Yi-ChyunWong@users.noreply.github.com>
2022-10-23 00:16:49 -0400

    Speech to text (from file)
    Running speech to text from a file. File has to be in the same directory as the .py programme.

--- speech_to_text.py
+++ speech_to_text.py
@@ -1,10 +1,16 @@
 import speech_recognition as sr
 r = sr.Recognizer()
-mic = sr.Microphone()
+lecture = sr.AudioFile('audio_files_harvard.wav')
+#mic = sr.Microphone()
 
-key = "running"
-print("Speak twat")
+with lecture as source:
+    r.adjust_for_ambient_noise(source)
+    audio = r.record(source)
 
+print(type(r.recognize_google(audio)))
+
+
+""""
 with mic as source:
     r.adjust_for_ambient_noise(source)
     audio = r.listen(source)
@@ -15,3 +21,5 @@ except sr.UnknownValueError:
     print("Could not understand audio")
 except sr.RequestError as e:
     print("Could not request results; {0}".format(e))
+
+"""
